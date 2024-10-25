4dbbfe6963dbeeb9a28cb0504bb644e9bfd89982
Patricia Ardila Parrado <74389328+patriardila@users.noreply.github.com>
2024-10-25 16:04:54 -0500

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>DEPENDENCIA TECNOLOGICA</title>
+</head>
+<body>
+    <h1>Titulo</h1>
+    <h2>Subtitulo</h2>
+    <h3>subtitulo</h3>
+    <p>Qué es Lorem Ipsum?
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+    <img src="https://www.observatoriorh.com/wp-content/uploads/2015/07/diez-d%C3%ADas-tecnolog%C3%ADas-y-dependencia.jpg" alt="Dependencia tecnológica">
+    <form action="" class="formulario">
+        <label for="">Nombre:</label>
+        <input type="text" placeholder="Ingresa nombre">
+        <label for="">Correo electrónico</label>
+        <input type="email" placeholder="Ingresa correo">
+        <label for="">Contraseña</label>
+        <input type="password" placeholder="Ingresa Contraseña">
+        <label for="">Adjunta imagen</label>
+        <input type="file" placeholder="Ingresa Una imagen">
+
+    </form>
+</body>
+</html>
