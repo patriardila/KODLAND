388cbba53e0e9caab49451747048889cd673d81c
Patricia Ardila Parrado <74389328+patriardila@users.noreply.github.com>
2025-04-17 17:33:05 -0500

    main1.py

--- main1.py
+++ main1.py
@@ -10,6 +10,7 @@
     <h1>Titulo</h1>
     <h2>Subtitulo</h2>
     <h3>subtitulo</h3>
+    <h3>subtitulo</h3>
     <p>Qué es Lorem Ipsum?
         Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
     <img src="https://www.observatoriorh.com/wp-content/uploads/2015/07/diez-d%C3%ADas-tecnolog%C3%ADas-y-dependencia.jpg" alt="Dependencia tecnológica">
